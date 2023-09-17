9cb3ce692f22ff607389e4a0dc3fd7c517e48037
sinu <65924192+sinui0@users.noreply.github.com>
2023-09-16 17:47:54 -0700

    update notarized_session fixture

--- tlsn/tlsn-fixtures/src/notarization/mod.rs
+++ tlsn/tlsn-fixtures/src/notarization/mod.rs
@@ -1,3 +1,13 @@
-mod session;
+use tlsn_core::session::NotarizedSession;
 
-pub use session::notarized_session;
+use p256::ecdsa::SigningKey;
+
+static NOTARIZED_SESSION_BYTES: &[u8] = include_bytes!("notarized_session.bin");
+
+pub fn notarized_session() -> NotarizedSession {
+    bincode::deserialize(NOTARIZED_SESSION_BYTES).expect("notarized session data is valid")
+}
+
+pub fn notary_signing_key() -> SigningKey {
+    SigningKey::from_slice(&[1; 32]).unwrap()
+}

0aebed12fa9689413ea8d9c638e05b20201894c8
mac <ryan.macarthur@gmail.com>
2024-09-26 15:42:39 -0700

    Delete cd-scripts/tee/docker/aws-nitro/#!/bin/bash nitro-cli build-enclave --docker-uri nitronotary:latest --docker-dir  directory

--- cd-scripts/tee/docker/aws-nitro/#!/bin/bash nitro-cli build-enclave --docker-uri nitronotary:latest --docker-dir /entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-nitro-cli build-enclave --docker-uri nitronotary:latest --docker-dir /enclave --output-file notary.eif --private-key ca-private.pem --signing-certificate ca.crt
-
-nitro-cli describe-eif --eif-path notary.eif
-nitro-cli run-enclave --cpu-count 2 --memory 256 --eif-path notary.eif

a9adec8216057602dc6d2ce085b617100370bbac
th4s <th4s@metavoid.xyz>
2025-01-13 11:40:40 +0100

    fix(prf): repair prf bench

--- crates/components/hmac-sha256/benches/prf.rs
+++ crates/components/hmac-sha256/benches/prf.rs
@@ -1,9 +1,9 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 use hmac_sha256::{MpcPrf, Prf, PrfConfig, Role};
-use mpz_common::executor::test_mt_executor;
+use mpz_common::executor::{mt::MTConfig, test_mt_executor};
 use mpz_garble::protocol::semihonest::{Evaluator, Generator};
-use mpz_ot::ideal::cot::ideal_cot_with_delta;
+use mpz_ot::ideal::cot::ideal_cot;
 use mpz_vm_core::{
     memory::{binary::U8, correlated::Delta, Array},
     prelude::*,
@@ -98,12 +98,12 @@ async fn prf() {
     let client_random = [69u8; 32];
     let server_random: [u8; 32] = [96u8; 32];
 
-    let (mut leader_exec, mut follower_exec) = test_mt_executor(128);
+    let (mut leader_exec, mut follower_exec) = test_mt_executor(128, MTConfig::default());
     let mut leader_ctx = leader_exec.new_thread().await.unwrap();
     let mut follower_ctx = follower_exec.new_thread().await.unwrap();
 
     let delta = Delta::random(&mut rng);
-    let (ot_send, ot_recv) = ideal_cot_with_delta(delta.into_inner());
+    let (ot_send, ot_recv) = ideal_cot(delta.into_inner());
 
     let mut leader_vm = Generator::new(ot_send, [0u8; 16], delta);
     let mut follower_vm = Evaluator::new(ot_recv);
@@ -136,22 +136,30 @@ async fn prf() {
         .set_server_random(&mut follower_vm, server_random)
         .unwrap();
 
+    #[allow(clippy::let_underscore_future)]
     let _ = leader_vm
         .decode(leader_output.keys.client_write_key)
         .unwrap();
+    #[allow(clippy::let_underscore_future)]
     let _ = leader_vm
         .decode(leader_output.keys.server_write_key)
         .unwrap();
+    #[allow(clippy::let_underscore_future)]
     let _ = leader_vm.decode(leader_output.keys.client_iv).unwrap();
+    #[allow(clippy::let_underscore_future)]
     let _ = leader_vm.decode(leader_output.keys.server_iv).unwrap();
 
+    #[allow(clippy::let_underscore_future)]
     let _ = follower_vm
         .decode(follower_output.keys.client_write_key)
         .unwrap();
+    #[allow(clippy::let_underscore_future)]
     let _ = follower_vm
         .decode(follower_output.keys.server_write_key)
         .unwrap();
+    #[allow(clippy::let_underscore_future)]
     let _ = follower_vm.decode(follower_output.keys.client_iv).unwrap();
+    #[allow(clippy::let_underscore_future)]
     let _ = follower_vm.decode(follower_output.keys.server_iv).unwrap();
 
     futures::join!(
@@ -176,10 +184,14 @@ async fn prf() {
     follower.set_cf_hash(&mut follower_vm, cf_hs_hash).unwrap();
     follower.set_sf_hash(&mut follower_vm, sf_hs_hash).unwrap();
 
+    #[allow(clippy::let_underscore_future)]
     let _ = leader_vm.decode(leader_output.cf_vd).unwrap();
+    #[allow(clippy::let_underscore_future)]
     let _ = leader_vm.decode(leader_output.sf_vd).unwrap();
 
+    #[allow(clippy::let_underscore_future)]
     let _ = follower_vm.decode(follower_output.cf_vd).unwrap();
+    #[allow(clippy::let_underscore_future)]
     let _ = follower_vm.decode(follower_output.sf_vd).unwrap();
 
     futures::join!(
